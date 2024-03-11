fe0cc7290a3b1a5d1162995f336179d29c760f62
bimlesharma <bimlesh.mdb@gmail.com>
2024-03-11 23:57:54 +0530

    new section added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import SectionThree from './components/SectionThree'
 import SectionFour from './components/SectionFour'
 import SectionFive from './components/SectionFive'
 import SectionSix from './components/SectionSix'
+import SectionSeven from './components/SectionSeven'
+import Footer from './components/Footer'
 
 function App () {
 
@@ -17,6 +19,8 @@ function App () {
       <SectionFour />
       <SectionFive />
       <SectionSix />
+      <SectionSeven />
+      <Footer />
     </>
   );
 }
