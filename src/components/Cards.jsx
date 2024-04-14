a9ba726086ee5ee588801208ba9a44efdad28788
bimlesharma <bimlesh.mdb@gmail.com>
2024-04-15 01:26:45 +0530

    section six made responsive

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,12 +3,33 @@ import CW from '../assets/CW.png';
 
 const Cards = ({heading, p, img}) => {
     return (
-        <div className='h-[25rem] text-white bg-ble-950 bg-cover bg-center bg-black w-[40rem] rounded-3xl'
-        style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.7), rgba(0,0,0,0.001)), url(${CW})` }}>
-            <h1 className='text-2xl py-5 pt-10 ml-5 z-10 font-bold'>Organized documentation</h1>
-            <p className='text-md ml-5 w-[60%]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, excepturi?</p>
+        <div className='h-[25rem]   font-bold text-white bg-cover bg-center bg-black rounded-3xl'
+            style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.7), rgba(0,0,0,0.001)), url(${img})` }}>
+            <h1 className='text-2xl py-5 pt-10 ml-5 z-10'>{heading}</h1>
+            <p className='text-md ml-5 w-[60%] pb-32'>{p}</p>
         </div>
     );
 };
 
 export default Cards;
+
+
+// import React from 'react';
+// import CW from '../assets/CW.png';
+
+// const Cards = ({ heading, p, img }) => {
+//     return (
+//         <div className='max-w-xl md:max-w-lg lg:max-w-xl xl:max-w-2xl mx-auto mb-8'>
+//             <div className='relative h-80 md:h-96 lg:h-96 xl:h-96 bg-black bg-cover bg-center rounded-3xl overflow-hidden'>
+//                 <div className='absolute inset-0 bg-gradient-to-b from-black to-transparent opacity-90'></div>
+//                 <div className='absolute inset-0 flex flex-col justify-center items-start px-6 md:px-8 lg:px-10 xl:px-12 text-white'>
+//                     <h1 className='text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold mb-4 md:mb-6'>{heading || 'Organized Documentation'}</h1>
+//                     <p className='text-sm md:text-md lg:text-lg xl:text-xl mb-6 md:mb-8'>{p || 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, excepturi?'}</p>
+//                 </div>
+//                 <img src={CW} alt='Card Background' className='hidden md:block absolute inset-0 w-full h-full object-cover opacity-40' />
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default Cards;
--- src/components/SectionSix.jsx
+++ src/components/SectionSix.jsx
@@ -1,15 +1,38 @@
-import React from 'react';
-import Cards from './Cards';
+// import React from 'react';
+// import Cards from './Cards';
+
+// function SectionSix() {
+//     return (
+//         <div className='min-h-[70vh] bg-white text-black'>
+//             <div className='mx-24 flex justify-between'>
+//             <Cards />
+//             <Cards />
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default SectionSix;
+
+import React from "react";
+import Cards from "./Cards";
+import CW from "../assets/CW.png";
 
 function SectionSix() {
-    return (
-        <div className='min-h-[70vh] bg-white text-black'>
-            <div className='mx-24 flex justify-between'>
-            <Cards />
-            <Cards />
-            </div>
-        </div>
-    );
+  return (
+    <div className=" bg-white text-black md:p-10 p-5 lg:p-20 gap-5 md:gap-8 lg:gap-20 flex flex-col md:flex-row justify-center items-center">
+      <Cards 
+        heading={'Organized Documentation'}
+        p={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, excepturi?'}
+        img = {CW}
+      />
+      <Cards 
+        heading={'Organized Documentation'}
+        p={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, excepturi?'}
+        img = {CW}
+      />
+    </div>
+  );
 }
 
 export default SectionSix;
