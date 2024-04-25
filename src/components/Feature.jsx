19a9eeabed2afbefaffbf4f43be91df3bc027782
bimlesharma <bimlesh.mdb@gmail.com>
2024-04-26 00:16:19 +0530

    minor fix

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -31,8 +31,8 @@ function Feature({ heading, para0, para1, img }) {
   return (
     <div className="py-10 px-6 md:px-10 lg:px-20 flex flex-col md:flex-row md:justify-center items-center ">
       {/* Left Section */}
-      <div className="left w-full md:w-1/2 md:mr-12 mb-8 md:mb-0">
-        <div className="max-w-lg">
+      <div className="left w-full md:w-1/2 lg:w-2/3 md:mr-12 mb-8 md:mb-0">
+        <div className="md:max-w-2xl ">
           <p className="font-bold text-gray-400 mb-6 md:mb-10">FEATURE</p>
           <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold leading-none text-grad">
             {heading}
@@ -44,7 +44,7 @@ function Feature({ heading, para0, para1, img }) {
       </div>
 
       {/* Right Section */}
-      <div className=" right md:w-1/2  w-full flex justify-center md:justify-end">
+      <div className=" right md:w-1/2 lg:w-1/3  w-full flex justify-center md:justify-end">
         <div
           className="w-full sm:w-96 rounded-xl h-80 sm:h-96 bg-cover bg-center md:hover:scale-[1.05] hover:shadow-lg transition duration-300 ease-in-out"
           style={{ backgroundImage: ` url(${img})` }}
