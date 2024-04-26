c105d335beca9274d96e7b14ac8fd9644f399003
bimlesharma <bimlesh.mdb@gmail.com>
2024-04-26 09:42:55 +0530

    minor fixes

--- src/components/BrandIcon.jsx
+++ src/components/BrandIcon.jsx
@@ -1,14 +1,3 @@
-// import React from "react";
-
-// const BrandIcon = ({ icon, height }) => {
-//     return (
-//         <div className="mx-10">
-//             <img src={icon} className={height} />
-//         </div>
-//     );
-// };
-
-// export default BrandIcon;
 
 import React from "react";
 
