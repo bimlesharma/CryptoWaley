0b6045abb4dc639a61a054d2845c4054dab1d1d0
bimlesharma <bimlesh.mdb@gmail.com>
2024-08-08 10:19:07 +0530

    pricing component setup

---
+++ src/components/Pricing-component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Pricing_component = () => {
+    return (
+        <div className=' border-8 border-slate-200  rounded-2xl min-h-[40vh] bg-white '>
+            
+            
+        </div>
+    );
+};
+
+export default Pricing_component;
