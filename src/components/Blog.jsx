2fd5d20ce2fa3d9aeff8441479495057c29bd3c7
bimlesharma <125790247+bimlesharma@users.noreply.github.com>
2024-12-19 15:26:10 +0000

    blog 3 publish

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from 'react';
 import b1img from '/assets/img/blog1/pepe.jpg';
 import b1img1 from '/assets/img/blog1/pepe1.png';
 import b2img from '/assets/img/blog2/ibw.jpg';
+import b3img from '/assets/img/blog3/ibw.jpg';
 
 const Blog = () => {
     const [posts, setPosts] = useState([]);
@@ -55,6 +56,67 @@ const Blog = () => {
                 
 
 
+                {/* BLOG NO. 3 */}
+                <div className='flex-col md:flex-row items-center gap-5 text-black bg-white border-4 rounded-xl mt-5 p-5'>
+
+                    <div className="content">
+                        <h1 className='text-3xl my-2 font-bold'>India Blockchain Week 2024 Conference Reveals Remarkable Web3 Ecosystem Growth</h1>
+                        <p className='text-sm'>By CryptoWaley on 19-12-2024</p>
+                        <p className=' my-6'>-The web3 ecosystem saw $462M in investments in the first nine months of 2024, an 82% growth from last year. With 1,000 startups, the world’s third-largest founder base, and 12% of global crypto developers, India is set to become the largest developer hub by 2027.</p>
+                        {/* <h2 className='text-2xl my-2 font-bold'>Introduction</h2> */}
+                        <p className=' mb-6'>BANGALORE, December 6th, 2024 – India Blockchain Week (IBW) Conference, the 2-day headline conference anchoring the India Blockchain Week series of 100+ Web3 events in Bangalore, is delighted to share that the week-long Web3 gathering has been a roaring success, solidifying India’s position as a global leader in Web3 adoption and innovation. The country’s entrepreneurial culture is setting global benchmarks for success, inspiring innovation, and charting a pioneering path for emerging nations.</p>
+                        <div className='flex justify-center'>
+                            <img className='rounded-xl border-2 my-2 md:w-[50%] w-full' src={b3img} alt="ibw img" />
+                        </div>
+                        {/* <h2 className='text-2xl my-2 font-bold'>Financial Landscape: Betting on Memes</h2> */}
+                        <p className='my-6'>The 2-day headline conference welcomed over 3500 enthusiasts, including investors, developers, and stakeholders, enabling them to explore the future of blockchain technology, digital innovation, and its impact on various industries. Throughout the week, there were 100+ side events offering builders and users a chance to receive advice from industry thought leaders, try out new products, and engage with community members.</p>
+                        {/* <h2 className='text-2xl my-2 font-bold'>Why PEPE Stands Out</h2> */}
+                        <p className='my-6'>The IBW2024 Conference featured 150+ visionary speakers including industry leaders such as Aptos Co-founder & CTO Avery Ching, Supra CEO & Co-Founder Joshua Tobkin, Polygon Co-founder Sandeep Nailwal, Monad CEO Keone Hon, and BitGo COO Chen Fang and many others, who shared their insights on the rapidly evolving Web3 landscape.</p>
+                        {/* <h2 className='text-2xl my-2 font-bold'>PEPE’s Unique Edge</h2> */}
+                        <p className='my-6'>Marking the occasion, Tak Lee, CEO & Managing Partner at Hashed Emergent, said, "India serves as a global microcosm, with its success stories inspiring global hope by showcasing how a nation reflecting the world's diverse realities can achieve population-scale impact. As India rapidly emerges as a key consumer of open network applications, we are deeply grateful to all our sponsors, speakers, and partners for making the India Blockchain Week 2024 Conference a tremendous success. We look forward to continuing this journey of innovation and collaboration, fostering a thriving blockchain ecosystem that bridges India with the world.”</p>
+                        {/* <h2 className='text-2xl my-2 font-bold'>Conclusion</h2> */}
+                        <p className='my-6'>Hashed Emergent, the organizers of the flagship IBW 2024 Conference, offered an exclusive preview of their upcoming annual Indian Web3 Landscape report. Built with its knowledge partners, the report provides an overview of India's evolving Web3 landscape, covering topics such as the rise in digital asset adoption, growth in Web3 gaming, and the burgeoning startup ecosystem with a focus on blockchain technology and increased developer participation. Some of the key highlights include:</p>
+
+                        <ul className='list-disc lg:px-10'>
+                            <li>India has the 3rd largest Web3 founder base in the world, housing 1000+ start-ups. </li>
+                            <li>In the first nine months of 2024, the ecosystem attracted $462 million in investments, reflecting an 82% growth from the previous year.</li>
+                            <li>Web3 gamers have almost doubled their wallet size as compared to that of Web2 gamers, with more Web2 gamers venturing into Web3 gaming.</li>
+                            <li>From Coinbase Ventures, Y Combinator, Animoca Brands, IOSG & LD Capital to the homegrown champions such as Hashed Emergent, CoinDCX, PeakXV, Borderless, and Alpha Wave are aggressively investing in Indian projects. It's a global vote of confidence in Indian Web3 innovation.</li>
+                            <li>Web3 gamers spend nearly double what traditional gamers do: $220 versus $120 annually. Not only that, they're more likely to be paying users – 64% compared to 56% in Web2.</li>
+                            <li>India saw the highest YoY developer growth at around 28%, adding more than 4.7M developers to GitHub in 2024.</li>
+
+                        </ul>
+
+
+                        <p className='my-6'>The conference also witnessed several partnerships, product launches, and other initiatives. Nazara Technologies and Lysto launched the Growth Protocol, a purpose-built Layer-1 blockchain for digital marketing and growth initiatives. Huddle01, the leading decentralised video conferencing platform, announced at IBW that it is reaching 4 million new users through a strategic partnership with SocialFi FanTV.</p>
+                        <p className='my-6'>Mudrex, a leading Indian crypto exchange, launched #LearnwithMudrex - India’s first multilingual crypto learning platform. It will partner with educators and top Indian investors to educate people in crypto as an asset class.</p>
+                        <p className='my-6'>The IBW2024 Conference organizers are thankful to all the attendees who traveled all the way to Bangalore to participate in the most significant Web3 event in India, as well as the sponsors Aptos, Supra, Polygon, Starknet and others who made it all possible. They also express their sincere gratitude to the 150+ speakers who took part in giving keynotes or panels at the conference.</p>
+                        <p>Next year, IBW Conference aims to partner with more stakeholders joining the conference and highlighting India's catalysing role in the global web3 industry.</p>
+                        <h2 className='text-2xl my-2 font-bold'>About India Blockchain Week (IBW)</h2>
+                        <p >India Blockchain Week (IBW) is a movement unifying the Indian web3 ecosystem and a platform to connect with the global community. It is India’s biggest pan-industry series of blockchain and web3 events, spanning from November 30th- December 8th 2024 across Bangalore, India. IBW is a week-long array of 100+ web3 events, including flagship events such as the headline IBW Conference, ETHIndia, FILBangalore and many other side-events, networking events, workshops, parties etc hosted by Indian & global blockchain industry stakeholders.</p>
+                        <p>The IBW Conference is the two-day headline event anchoring India Blockchain Week (IBW) on 4-5 December 2024 at the Sheraton Grand, Bangalore and hosted by Hashed Emergent - a web3 venture capital firm dedicated to accelerating the mass adoption of web3 in India.</p>
+                        <p>For more information about IBW, visit: 
+                            <a className='text-blue-600 underline' href="https://indiablockchainweek.com/"> Website </a>
+                            <a className='text-blue-600 underline' href="https://twitter.com/IBWofficial">Twitter </a>
+                            <a className='text-blue-600 underline' href="https://www.linkedin.com/company/ibwofficial/">LinkedIn </a>
+                            <a className='text-blue-600 underline' href="https://t.me/ibw_official">Telegram</a>
+                        </p>
+
+                        <h2 className='text-2xl my-2 font-bold'><a href="https://www.hashedem.com/">About Hashed Emergent</a></h2>
+                        <p>Hashed Emergent is the venture capital arm of Hashed, backing builders from India and emerging markets shaping the on-chain frontier. It invests at the earliest stages of a web3 startup's journey, using its expertise, experience, and network to amplify their growth and success. The team is rooted in the cultural hubspots of web3 in emerging markets, spanning across Bangalore, Seoul, Singapore, Lagos and Dubai. Hashed Emergent drives web3 focused ecosystem and community expansion in these markets through its purpose-built initiatives. </p>
+                        
+                        <h2 className='text-2xl my-2 font-bold'>For media queries, Please contact: </h2>
+                        <p>Ridhima Bhasin - press@indiablockchainweek.com</p>
+
+                        
+
+                    </div>
+                    <div className='flex justify-center'>
+                        {/* <img className='rounded-xl my-2 md:w-[50%] w-full' src={b1img1} alt="PEPE: The King of MemesCoins" /> */}
+                    </div>
+                    {/* <p>You can buy cryptocurrencies like PEPE on <a className='text-blue-700 font-bold' href="https://www.bitmart.com/invite/7MC9jA/en" target='_blank'>Bitmart.</a></p> */}
+
+                </div>
                 {/* BLOG NO. 2 */}
                 <div className='flex-col md:flex-row items-center gap-5 text-black bg-white border-4 rounded-xl mt-5 p-5'>
 
